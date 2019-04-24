40198a6d2a4476284191986fb08cf511f3c21a6c
sufei <oneacorn@163.com>
2019-04-24 14:23:12 +0800

    finish

--- app/src/main/java/com/acorn/overlaycardviewpager/MainActivity.java
+++ app/src/main/java/com/acorn/overlaycardviewpager/MainActivity.java
@@ -1,13 +1,26 @@
 package com.acorn.overlaycardviewpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager vp;
+    private String[] imgUrls = new String[]{"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094759217&di=943686daea0415763a8364d3ac0d8233&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fe7abe6079c12527f838195f6b0de9c88bdb0b6cb.jpg"
+            , "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094800005&di=ba8f6dbb833600fa76af7850265d1309&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0145b25cade37fa801214168567afc.jpg%401280w_1l_2o_100sh.jpg"
+            , "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094815760&di=1abe539eb4691346c07dd44a6bba7383&imgtype=0&src=http%3A%2F%2Fpic.xoyo.com%2Fbbs%2F2010%2F11%2F30%2F10113010300bdf68f9f96b70e4.jpg"
+            , "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094832094&di=ceb750ce77ad8699859603b80f6479f5&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F015fa95c67b3d7a801203d22e96898.jpg%401280w_1l_2o_100sh.jpg"
+            , "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094846679&di=fb63907df03214def9e4576149ac1b8e&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2Fc9257b7638df71ad54d17de9cc92034741c86236.jpg"
+            , "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556094860210&di=56a4eba83e1fa1fee69787b35f5b2806&imgtype=0&src=http%3A%2F%2Fi0.hdslb.com%2Fbfs%2Farticle%2F5d3129cc0cc155f696a68eec5356d9387f4c517d.jpg"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vp = findViewById(R.id.example_vp);
+        OverlayPageAdapter adapter = new OverlayPageAdapter(this);
+        adapter.setImgUrlsAndBindViewPager(vp, imgUrls);
+        vp.setAdapter(adapter);
+        vp.setCurrentItem(100000);
     }
 }
---
+++ app/src/main/java/com/acorn/overlaycardviewpager/OverlayPageAdapter.java
@@ -0,0 +1,86 @@
+package com.acorn.overlaycardviewpager;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.lang.ref.WeakReference;
+
+public class OverlayPageAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+    private String[] imgUrls;
+    private WeakReference<Bitmap>[] bitmaps;
+    private RequestOptions mRequestOptions;
+
+    public OverlayPageAdapter(Context context) {
+        this.context = context;
+        this.mRequestOptions = new RequestOptions()
+                .error(R.drawable.ic_launcher_foreground)
+                .placeholder(R.drawable.ic_launcher_foreground);
+        inflater = LayoutInflater.from(context);
+    }
+
+    public void setImgUrlsAndBindViewPager(ViewPager vp, String[] imgUrls) {
+        this.imgUrls = imgUrls;
+        if (imgUrls != null && imgUrls.length > 0) {
+            bitmaps = new WeakReference[imgUrls.length];
+            vp.setOffscreenPageLimit(3);
+            OverlayTransformer transformer = new OverlayTransformer(3);
+            vp.setPageTransformer(true, transformer);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        if (null == imgUrls)
+            return 0;
+        if (imgUrls.length <= 1)
+            return imgUrls.length;
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        final int p = position % imgUrls.length;
+        final String imgUrl = imgUrls[p];
+        View view = inflater.inflate(R.layout.item_viewpager, null);
+        final ImageView iv = view.findViewById(R.id.example_iv);
+        if (null != bitmaps && null != bitmaps[p] && null != bitmaps[p].get()) {
+            iv.setImageBitmap(bitmaps[p].get());
+        }
+        Glide.with(context).asBitmap().load(imgUrl).apply(mRequestOptions).into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                bitmaps[p] = new WeakReference<Bitmap>(resource);
+                iv.setImageBitmap(resource);
+            }
+        });
+        view.setTag(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
